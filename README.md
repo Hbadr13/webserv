87696f01b7e45f53240a9f398a02d6320e7253cc
hbadr13 <hamza1937badr@gmail.com>
2023-01-22 11:04:20 +0100

    http server

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ https://github.com/sFinOe/HTTP-Web-Server#readme
 
 ![Screenshot](https://media.geeksforgeeks.org/wp-content/uploads/20220330131350/StatediagramforserverandclientmodelofSocketdrawio2-448x660.png)
 
+What is **OSI**
+[![Watch the video](https://youtu.be/9zqHMl9_s5k]
 
 **Sockets**
 https://www.geeksforgeeks.org/socket-programming-cc/
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = (char *) "Hello from server";
+    char *hello = (char *)"Hello from server";
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     address.sin_family = AF_INET;
@@ -17,9 +17,7 @@ int main(int argc, char const *argv[])
     listen(server_fd, 3);
     new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
     std::cout << "connected" << std::endl;
-    valread = read(new_socket, buffer, 20000);
-    std::cout << "connected" << std::endl;
-
+    valread = read(new_socket, buffer, 1000);
     printf("%s", buffer);
     return 0;
 }
